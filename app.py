7fa60495287b7f0a2b651dcf834df7bed4a91e56
TravisRivera14 <riveraobandot@icloud.com>
2026-06-21 01:33:26 -0600

    main

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'iCareTechCR_Master_Key_2026
 
 app.config.update(
     SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_SAMESITE='Lax',
+    SESSION_COOKIE_SAMESITE='None',
     SESSION_COOKIE_HTTPONLY=True
 )
 
